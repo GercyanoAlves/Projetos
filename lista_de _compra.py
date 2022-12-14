5af61f71d4a9b91bc83561ee2a3ac681fb66bd8d
GercyanoAlves <gercyanoalves@gmail.com>
2022-12-14 19:29:46 -0300

    Ajustes

--- lista_de _compra.py
+++ lista_de _compra.py
@@ -1,3 +1,4 @@
+import os
 itens = []
 preco = []
 qtd = []
@@ -13,19 +14,29 @@ print("###############################################")
 print("###############################################\n")
 
 while True:
-    sair = input("Digite -1 para Sair ou ENTER para continuar: \n")
-    if sair == '-1':
+    opcao = input("Digite sair minusculo para encerrar ou ENTER para continuar: ")
+    if opcao == 'sair':
         break
-    itens.append(input("Nome do produto: "))
-    qtd = float(input('Quantidade: '))
-    preco = float(input("Preco do produto: "))
-    total_preco.append(float(qtd*preco))
-    
-    print("Total R$ %.2f"%total_preco[y])
-    print()
-    y += 1
+    try:
+        itens.append(input("Nome do produto: "))
+        qtd = float(input('Quantidade: '))
+        preco = float(input("Preco do produto: "))
+        total_preco.append(float(qtd*preco))
+        
+        print("Total R$ %.2f"%total_preco[y])
+        print()
+        y += 1
+    except ValueError:
+        os.system("clear")
+        print("""\nPara adicionar o produto na lista veja o que é obrigatório.
+        1- Produto pode ficar em branco
+        2- Colocar quantidade é obrigatoria
+        3- Colocar preço é obrigatório
+        4- Não pode usar VÍRGULA e tampouco LETRAS\n""")
 
-    
+for indice, compras in enumerate(itens):
+    print(indice, compras)
+print("Produtos listada acima estão no carrinho")
 print("Valor total da compra é de R$%.2f"%sum(total_preco))
 
 print("###############################################")
